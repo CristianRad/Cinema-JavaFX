aaba609ec2e45c4bdbf0eeccc451eb0376bb6ffd
CristianRad <cristian.mihai.rad@gmail.com>
2019-03-30 16:21:33 +0200

    Cinema GUI

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+
 public class Main extends Application {
 
     @Override
@@ -37,6 +39,15 @@ public class Main extends Application {
         filmService.addFilm("4","Mr. Bean",2002,41.0,true);
         filmService.addFilm("5","Matrix",2003,29.0,false);
         filmService.addFilm("6","Armageddon",1998,50.0,true);
+
+        clientService.addClient("1","Robert","Bumb","1920921126202", LocalDate.of(1992,9,21), LocalDate.of(2016,8,4), 39);
+        clientService.addClient("3","Iulian","Pop","1951010126212", LocalDate.of(1995,10,10), LocalDate.of(2015,9,9), 45);
+        clientService.addClient("6","Alin","Vlad","1930205126234", LocalDate.of(1993,2,5), LocalDate.of(2018,12,17), 16);
+        clientService.addClient("7","Adrian","Popescu","1900109124936", LocalDate.of(1990,1,9), LocalDate.of(2013,6,11), 0);
+        clientService.addClient("9","Darius","Voicu","1881020124948", LocalDate.of(1988,10,20), LocalDate.of(2014,5,28), 30);
+        clientService.addClient("14","Maria","Goga","2820423126207", LocalDate.of(1982,4,23), LocalDate.of(2008,11,3), 68);
+        clientService.addClient("19","Georgiana","Moga","2890531124935", LocalDate.of(1989,5,31), LocalDate.of(2012,3,18), 53);
+        clientService.addClient("20","Mihai","Dita","1940930126190", LocalDate.of(1994,9,30), LocalDate.of(2012,2,29), 22);
 
         MainController mainController = fxmlLoader.getController();
         mainController.setServices(filmService, clientService, reservationService);
--- src/UI/MainController.java
+++ src/UI/MainController.java
@@ -1,5 +1,6 @@
 package UI;
 
+import Domain.Client;
 import Domain.Film;
 import Service.ClientService;
 import Service.FilmService;
@@ -9,7 +10,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainController {
 
@@ -19,20 +28,21 @@ public class MainController {
     public TableColumn colFilmYear;
     public TableColumn colFilmPrice;
     public TableColumn colFilmOnScreen;
-    public TextField txtFilmId;
-    public TextField txtFilmTitle;
-    public TextField txtFilmYear;
-    public TextField txtFilmPrice;
-    public CheckBox chkOnScreen;
-    public Button btnAddFilm;
-    public Button btnUpdateFilm;
-    public Button btnRemoveFilm;
+    public TableView tblClients;
+    public TableColumn colClientId;
+    public TableColumn colClientName;
+    public TableColumn colClientSurname;
+    public TableColumn colClientCnp;
+    public TableColumn colClientBirthday;
+    public TableColumn colClientRegistrationDay;
+    public TableColumn colClientPoints;
 
     private FilmService filmService;
     private ClientService clientService;
     private ReservationService reservationService;
 
     private ObservableList<Film> films = FXCollections.observableArrayList();
+    private ObservableList<Client> clients = FXCollections.observableArrayList();
 
     public void setServices(FilmService filmService, ClientService clientService, ReservationService reservationService) {
         this.filmService = filmService;
@@ -45,34 +55,47 @@ public class MainController {
         Platform.runLater(() -> {
             films.addAll(filmService.getAllFilms());
             tblFilms.setItems(films);
+            clients.addAll(clientService.getAllClients());
+            tblClients.setItems(clients);
         });
     }
 
-    public void btnAddFilmClick(ActionEvent actionEvent) {
+    public void upsertFilmSettings(FXMLLoader fxmlLoader, String title) {
         try {
-            String id = txtFilmId.getText();
-            String title = txtFilmTitle.getText();
-            int year = Integer.parseInt(txtFilmYear.getText());
-            double price = Double.parseDouble(txtFilmPrice.getText());
-            boolean onScreen = chkOnScreen.isSelected();
+            Scene scene = new Scene(fxmlLoader.load(),235,200);
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            FilmAddController controller = fxmlLoader.getController();
+            controller.setService(filmService);
+            stage.showAndWait();
 
-            filmService.addFilm(id, title, year, price, onScreen);
             films.clear();
             films.addAll(filmService.getAllFilms());
-        } catch (MainControllerException error) {
-            Common.showValidationError(error.getMessage());
+        } catch (IOException error) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new window: Film Add/Update", error);
         }
+    }
+
+    public void btnAddFilmClick(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("filmAdd.fxml"));
+        upsertFilmSettings(fxmlLoader, "Film Add");
     }
 
     public void btnUpdateFilmClick(ActionEvent actionEvent) {
-        try {
-            String id = txtFilmId.getText();
-            String title = txtFilmTitle.getText();
-            int year = Integer.parseInt(txtFilmYear.getText());
-            double price = Double.parseDouble(txtFilmPrice.getText());
-            boolean onScreen = chkOnScreen.isSelected();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("filmUpdate.fxml"));
+        upsertFilmSettings(fxmlLoader, "Film Update");
+    }
 
-            filmService.updateFilm(id, title, year, price, onScreen);
+    public void btnRemoveFilmClick(ActionEvent actionEvent) {
+        Film selected = (Film) tblFilms.getSelectionModel().getSelectedItem();
+        if (selected != null)
+        try {
+            filmService.removeFilm(selected.getId());
             films.clear();
             films.addAll(filmService.getAllFilms());
         } catch (MainControllerException error) {
@@ -80,16 +103,47 @@ public class MainController {
         }
     }
 
-    public void btnRemoveFilmClick(ActionEvent actionEvent) {
+    public void upsertClientSettings(FXMLLoader fxmlLoader, String title) {
         try {
-            String id = txtFilmId.getText();
+            Scene scene = new Scene(fxmlLoader.load(),250,280);
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            ClientAddController controller = fxmlLoader.getController();
+            controller.setService(clientService);
+            stage.showAndWait();
 
-            filmService.removeFilm(id);
-            films.clear();
-            films.addAll(filmService.getAllFilms());
-        } catch (MainControllerException error) {
-            Common.showValidationError(error.getMessage());
+            clients.clear();
+            clients.addAll(clientService.getAllClients());
+        } catch (IOException error) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE,"Failed to create new window: Client Add/Update", error);
         }
+    }
+
+    public void btnAddClientClick(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("clientAdd.fxml"));
+        upsertClientSettings(fxmlLoader, "Client Add");
+    }
+
+    public void btnUpdateClientClick(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("clientUpdate.fxml"));
+        upsertClientSettings(fxmlLoader, "Client Update");
+    }
+
+    public void btnRemoveClientClick(ActionEvent actionEvent) {
+        Client selected = (Client) tblClients.getSelectionModel().getSelectedItem();
+        if (selected != null)
+            try {
+                clientService.removeClient(selected.getId());
+                clients.clear();
+                clients.addAll(clientService.getAllClients());
+            } catch (MainControllerException error) {
+                Common.showValidationError(error.getMessage());
+            }
     }
 
 }
